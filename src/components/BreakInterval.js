0cef6b6ed2c6f3d826d203c9830cafac9b294ffb
Hyung-Min-Chong <michaelminchong@gmail.com>
2020-09-15 15:51:38 +0100

    pomodoro clock first commit

---
+++ src/components/BreakInterval.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function BreakInterval(props) {
+    function decreasedCounter() {
+        if (props.breakInterval === 1) {
+            return;
+        }
+        props.decreaseBreak()
+    }
+
+     function increaseCounter() {
+         if (props.breakInterval === 60) {
+             return; 
+         }
+
+         props.increaseBreak();
+     }
+
+    return (
+        <section>
+            <h4>Break Length</h4>
+        <section className="interval-container">
+            <button disabled={props.isPlay === true ? "disabled" : ""} onClick={decreasedCounter}>Down</button>
+            <p className="interval-length">{props.breakInterval}</p>
+            <button disabled={props.isPlay === true ? "disabled" : ""} onClick={increaseCounter}>Up</button>
+        </section>
+        </section>
+    );
+}
+
+export default BreakInterval;
